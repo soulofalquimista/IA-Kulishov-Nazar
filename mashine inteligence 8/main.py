364fe08a1602f00bab246337a302351b4e6dcedf
Назар Кулішов <158081111+soulofalquimista@users.noreply.github.com>
2026-03-27 19:25:58 +0200

    Update main.py

--- mashine inteligence 8/main.py
+++ mashine inteligence 8/main.py
@@ -2,9 +2,7 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 
-# =====================================
-# 1. Дані
-# =====================================
+
 
 transactions = [
     ['bread', 'milk'],
@@ -14,9 +12,7 @@ transactions = [
     ['bread', 'milk', 'diapers', 'cola']
 ]
 
-# =====================================
-# 2. Перетворення в DataFrame
-# =====================================
+
 
 te = TransactionEncoder()
 te_data = te.fit(transactions).transform(transactions)
@@ -26,18 +22,14 @@ df = pd.DataFrame(te_data, columns=te.columns_)
 print("Дані:")
 print(df)
 
-# =====================================
-# 3. Пошук частих наборів
-# =====================================
+
 
 frequent_itemsets = apriori(df, min_support=0.4, use_colnames=True)
 
 print("\nЧасті набори:")
 print(frequent_itemsets)
 
-# =====================================
-# 4. Генерація правил
-# =====================================
+
 
 rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.6)
 
